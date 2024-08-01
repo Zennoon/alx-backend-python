e65cc30b1709903172f3842eed5706682ce63625
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-08-01 18:39:14 +0300

    Write an annotated function that accepts two strings and returns their concatenation

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Contains:
+    Functions
+    =========
+    concat - accepts two strings and concatenates them
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    return str1 + str2
