3adb8b371e0bf71179a0917b00f002b37f01fd5f
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-08-01 18:45:21 +0300

    Write a type annotated function that accepts a floor and returns its floor (an int)

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Contains:
+    Functions
+    =========
+    floor - accepts a float and returns its floor (an int)
+"""
+
+
+def floor(n: float) -> int:
+    """Accepts a float and returns its floor (an int)"""
+    return n.__floor__()
