f1f521c420a44292a4b2f4628d19b0b5826f8b15
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-08-01 18:51:31 +0300

    Write a type-annotated function that accepts a float and returns its string representation

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Contains:
+    Functions
+    =========
+    to_str - accepts a float and returns its string representation
+"""
+
+
+def to_str(n: float) -> str:
+    """Accepts a float and returns its string representation"""
+    return str(n)
