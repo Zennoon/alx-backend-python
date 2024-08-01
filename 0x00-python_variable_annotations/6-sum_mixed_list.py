ce6cb924aebd11e42f9c5063df7992267dcb3803
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-08-01 19:13:02 +0300

    Fix name of function parameter

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -6,11 +6,11 @@ Contains:
     sum_mixed_list - accepts a list of ints and floats and returns their sum
     (as a float)
 """
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_list: List[float | int]) -> float:
+def sum_mixed_list(mxd_lst: List[int | float]) -> float:
     """
     Accepts a list of ints and floats and returns their sum (as a float)
     """
-    return float(sum(mxd_list))
+    return float(sum(mxd_lst))
