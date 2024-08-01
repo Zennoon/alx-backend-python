85cbb4b43d1a88fcc762bcff7066bf588672d44b
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-08-01 18:55:52 +0300

    Write a script that defines and annotates variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Contains:
+    Globals
+    =======
+    a - an integer
+    pi - a float
+    i_understand_annotations - a boolean
+    school - a string
+"""
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
