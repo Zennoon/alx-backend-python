52bd6f778127f839eff23a66190dac91b5c7926f
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-08-05 23:24:41 +0300

    Write a function that uses the time module's benchmarking perf_counter function to measure the time taken by wait_n

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Contains:
+    Functions
+    =========
+    measure_time - measures time per call to the async coroutine wait_n
+"""
+import asyncio
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    Calls wait_n with the given arguments and measures the
+    average time it takes for wait_n to execute one wait_random
+    call
+    """
+    start = time.perf_counter()
+    wait_n(n, max_delay)
+    end = time.perf_counter()
+    return ((end - start) / n)
