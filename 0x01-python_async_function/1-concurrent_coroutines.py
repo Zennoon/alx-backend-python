08286dcdbf738e3b3b2ce08186ffc1c1266150bc
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-08-05 22:46:15 +0300

    Remove debugging print statements

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -18,7 +18,3 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     Calls another coroutines n times passing the given max_delay value
     """
     return (await asyncio.gather(*(wait_random(max_delay) for _ in range(n))))
-
-print(asyncio.run(wait_n(5, 5)))
-print(asyncio.run(wait_n(10, 7)))
-print(asyncio.run(wait_n(10, 0)))
