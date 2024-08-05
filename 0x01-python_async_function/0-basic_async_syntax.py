0f0170fdaf02c9d1bae92a44b62c5fc3cd2846df
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-08-05 22:23:57 +0300

    Write an async coroutine that accepts an integer and sleeps for a float time between 0 and given int

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Contains:
+    Functions
+    =========
+    wait_random - an async coroutine that accepts an integer
+    and waits/sleeps for a random time between 0 and given integer 
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Sleeps for a randomly generated float seconds between 0 and given integer
+    """
+    rand = random.uniform(0, max_delay)
+    await asyncio.sleep(rand)
+    return (rand)
---
+++ 0x01-python_async_function/README.md
@@ -0,0 +1,2 @@
+This directory was created on August 5, 2024 for the ALX Backend track
+project regarding Python's Async IO implementation.
