992a3d1d3a9f2054cc4daab3a4470c54934cb8d5
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-08-06 18:59:46 +0300

    Write an async generator yielding a random float
    Create an async generator that loops 10 times and each time
    sleeps 1 second and then yields a random value between 1 and 10

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Contains:
+    Functions/Coroutines
+    ====================
+    async_generator - An async generator that yields random
+    values ten times and sleeps for a second
+"""
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    Generates/yields a random value between 1 and 10 ten times
+    and each time sleeps for 1 second
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
---
+++ 0x02-python_async_comprehension/README.md
@@ -0,0 +1,2 @@
+This directory was created on August 6, 2024 for the ALX SE Backend track
+project about async comprehensions along with async generators, and iterators.
